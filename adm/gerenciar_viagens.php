2d4bacfb5d8abd31b6e04ab470613c3f650771d8
Erubbo <eros.erubbo@gmail.com>
2022-08-10 21:23:10 -0300

    add ADM

---
+++ adm/gerenciar_viagens.php
@@ -0,0 +1,74 @@
+<?php
+include '../backend/conexao.php';
+
+
+try {
+    $sql = "SELECT * FROM tb_viagens";
+
+    $command = $conn->prepare($sql);
+
+    $command->execute();
+
+    $dados = $command->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $error) {
+    echo $error->getMessage();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt_bt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciamento das viagens</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="contaienr">
+        <h3>Gerenciar Viagens</h3>
+        <div class="tabela">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Local</th>
+                    <th>Valor</th>
+                    <th>Descrição</th>
+                    <th>Alterar</th>
+                    <th>Deletar</th>
+                </tr>
+               <?php
+               foreach($dados as $d):
+               ?>
+                <tr>
+                    <td><?php echo $d['id']?></td>
+                    <td><?php echo $d['titulo']?></td>
+                    <td><?php echo $d['local']?></td>
+                    <td>R$ <?php echo $d['valor']?></td>
+                    <td><?php echo $d['desc']?></td>
+                    <td>
+                        Alterar
+                    </td>
+                    <td>
+                        <a href="../backend/_deletar_viagens.php?id=<?php echo $d['id']?>">Deletar</a>
+                    </td>
+                </tr>
+<?php endforeach ?>
+            </table>
+
+
+
+
+        </div>
+    </div>
+<?php
+$conn = null;
+?>
+</body>
+
+</html>
---
+++ backend/_deletar_viagens.php
@@ -0,0 +1,24 @@
+<?php
+include 'conexao.php';
+
+try{
+// captura o id enviado via get e armazena em uma variavel
+
+$id = $_GET['id'];
+
+$sql = "DELETE from tb_viagens where id=$id";
+
+$command = $conn->prepare ($sql);
+
+$command-> execute();
+
+header('location: ../adm/gerenciar_viagens.php');
+
+
+}catch(PDOException $error){
+
+    echo $error->getMessage();
+
+}
+
+?>
