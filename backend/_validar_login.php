0857b5b184d8f5cf14c0226c0bd7532af412ab22
Erubbo <eros.erubbo@gmail.com>
2022-08-17 20:02:41 -0300

    Login finalizado

--- backend/_validar_login.php
+++ backend/_validar_login.php
@@ -5,7 +5,12 @@ try{
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql = "SELECT * from tb_login where email = '$email' AND senha= '$senha'";
+$sql = "SELECT * from 
+tb_login 
+where email = '$email' 
+AND senha= '$senha'
+AND ATIVO=1";
+
 
 $command = $conn->prepare($sql);
 
@@ -13,7 +18,17 @@ $command ->execute();
 
 $dados = $command ->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($dados);
+// var_dump($dados);
+// VERIFICA SE EXISTEM REGISTROS DENTRO DA VARIAVEL DADOS
+
+if($dados != null ){
+    header('Location: ../adm/gerenciar_viagens.php');
+}else{
+
+    // header('location: ../adm/index.html');
+    
+
+}
 
  }catch(PDOException $error){
     echo $error->getMessage();
